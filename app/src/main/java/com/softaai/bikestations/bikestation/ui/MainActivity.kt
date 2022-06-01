fed8daeeed621063552af3bc901f0d3b56fbfc37
amol pawar <amoljp19@gmail.com>
2022-06-01 13:02:53 +0530

    resolved hilt minor bugs

--- app/src/main/java/com/softaai/bikestations/bikestation/ui/MainActivity.kt
+++ app/src/main/java/com/softaai/bikestations/bikestation/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.softaai.bikestations.bikestation.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.softaai.bikestations.bikestation.ui.theme.BikeStationsTheme
+import com.softaai.bikestations.bikestation.viewmodel.MainViewModel
+import com.softaai.bikestations.data.network.State
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    val mViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +32,35 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                }
+
+                getBikeStationsList()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        observeBikeStationsLiveData()
+    }
+
+    private fun getBikeStationsList() = mViewModel.getBikeStationsList()
+
+    private fun observeBikeStationsLiveData() {
+
+        mViewModel.bikeStationsLiveData.observe(this) { state ->
+            when (state) {
+                is State.Loading -> Toast.makeText(
+                    applicationContext,
+                    "Loading...",
+                    Toast.LENGTH_SHORT
+                ).show()
+                is State.Success -> {
+                    Toast.makeText(applicationContext, " " + state.data, Toast.LENGTH_SHORT).show()
+                }
+                is State.Error -> {
+                    Toast.makeText(applicationContext, " " + state.message, Toast.LENGTH_SHORT)
+                        .show()
                 }
             }
         }
